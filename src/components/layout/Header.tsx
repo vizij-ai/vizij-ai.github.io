c9c6c8151cec87cf518737bc6ac18a9b7141fd84
Andrew Schoen <schoen.andrewj@gmail.com>
2025-12-13 11:16:16 -0800

    Use absolute path for brand.svg in icon and header
    Updated the favicon and header logo image sources to use an absolute path ('/brand.svg') instead of a relative one. This ensures the correct asset is loaded regardless of the current route.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -114,7 +114,7 @@ export default function Header({
           href={homeUrl()}
         >
           <div title={siteConfig.title}>
-            <img src="brand.svg" className="size-5" />
+            <img src="/brand.svg" className="size-5" />
           </div>
           <strong className="max-[320px]:hidden lowercase text-base hidden lg:block z-10 mb-0.5 ms-2 lg:text-base xl:text-xl hover:opacity-90 whitespace-nowrap">
             {siteConfig.title}
