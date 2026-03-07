20bf9a1102e13f9c51fa97fdf6e96c2a50e1fb62
AndrewJSchoen <AndrewJSchoen@users.noreply.github.com>
2026-03-07 22:24:16 +0000

    Deploy preview for PR 9 🛫

---
+++ pr-preview/pr-9/_astro/ResearchCard.CqJyqsb5.js
@@ -0,0 +1,2 @@
+import{j as s}from"./MobileNavButton._gUN0GSo.js";import{I as o,c as t,d as e,e as d}from"./ItemCard.CDTi-izd.js";const c=({hardwareId:r,data:a})=>s.jsx(o,{...t(r,a)}),m=({softwareId:r,data:a})=>s.jsx(o,{...e(r,a)}),x=({researchId:r,data:a})=>s.jsx(o,{...d(r,a)});export{c as H,x as R,m as S};
+//# sourceMappingURL=ResearchCard.CqJyqsb5.js.map
