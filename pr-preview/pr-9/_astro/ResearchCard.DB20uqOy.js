1324939f5d55e1216e6383d51eea0df39de49ff7
AndrewJSchoen <AndrewJSchoen@users.noreply.github.com>
2026-03-09 13:40:13 +0000

    Deploy preview for PR 9 🛫

--- pr-preview/pr-9/_astro/ResearchCard.DB20uqOy.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./SearchProvider.DxBlJlkD.js";import{I as o,c as t,d as e,e as d}from"./ItemCard.CnUkxJM-.js";const c=({hardwareId:r,data:a})=>s.jsx(o,{...t(r,a)}),m=({softwareId:r,data:a})=>s.jsx(o,{...e(r,a)}),x=({researchId:r,data:a})=>s.jsx(o,{...d(r,a)});export{c as H,x as R,m as S};
-//# sourceMappingURL=ResearchCard.DB20uqOy.js.map
