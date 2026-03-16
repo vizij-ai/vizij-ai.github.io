a4ab667d9d778ae9fb0c17a6d42853ba2cb96b4b
victorpaleologue <victorpaleologue@users.noreply.github.com>
2026-03-16 00:29:10 +0000

    Remove preview for PR 16 🛬

--- pr-preview/pr-16/_astro/ResearchCard.OvHP2Of2.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./SearchProvider.e654cDZE.js";import{I as o,c as t,d as e,e as d}from"./ItemCard.acRWTU4y.js";const c=({hardwareId:r,data:a})=>s.jsx(o,{...t(r,a)}),m=({softwareId:r,data:a})=>s.jsx(o,{...e(r,a)}),x=({researchId:r,data:a})=>s.jsx(o,{...d(r,a)});export{c as H,x as R,m as S};
-//# sourceMappingURL=ResearchCard.OvHP2Of2.js.map
