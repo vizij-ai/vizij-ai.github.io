1faa8d0d97af2353f8283ac5b11cbab301abf302
AndrewJSchoen <AndrewJSchoen@users.noreply.github.com>
2026-09-11 17:51:34 +0000

    Remove preview for PR 25 🛬

--- pr-preview/pr-25/_astro/ResearchCard.DcDd_b13.js
+++
@@ -1,2 +0,0 @@
-import{j as s}from"./SearchProvider.BA6szxXs.js";import{I as o,d as t,e,f as d}from"./ItemCard.Cdb7263k.js";const m=({hardwareId:r,data:a})=>s.jsx(o,{...t(r,a)}),x=({softwareId:r,data:a})=>s.jsx(o,{...e(r,a)}),c=({researchId:r,data:a})=>s.jsx(o,{...d(r,a)});export{m as H,c as R,x as S};
-//# sourceMappingURL=ResearchCard.DcDd_b13.js.map
